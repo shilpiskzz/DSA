a5c80a3e50ef874d59a50da3866729e241c47a15
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2024-09-07 13:41:06 +0530

    update vectors.cpp

---
+++ Trees/vectors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    cout << v[0] << " " << v.at(0) << endl;
+    cout << v[3] << " " << v.at(3) << endl;
+
+    /*
+    cout << "using indexing : a garbage value " << v[5] << endl;
+    cout << "using at function : " << v.at(5) << endl;
+    */
+
+    cout << "size: " << v.size() << endl;
+    v.pop_back();
+    cout << "size: " << v.size() << endl;
+
+    /*
+    dynamically
+    vector<int> *vp = new vector<int>();
+    */
+}
