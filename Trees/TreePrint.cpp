9f8a0273f6e228a3f7410284fe00977443a63733
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2024-09-27 19:58:16 +0530

    update treePrint.cpp

---
+++ Trees/TreePrint.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#include "treeNode.h"
+
+void printTree(TreeNode<int> *root)
+{
+
+    // edge case (not the base case)
+    // base case is the case that comes after completing the recurssion cycle in the end
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << ":";
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
+int main()
+{
+    TreeNode<int> *rootNode = new TreeNode<int>(10); // this is not a vector , yeh ek treenode type ka object hai
+    TreeNode<int> *node1 = new TreeNode<int>(20);
+    TreeNode<int> *node2 = new TreeNode<int>(30);
+
+    rootNode->children.push_back(node1);
+    rootNode->children.push_back(node2);
+    printTree(rootNode);
+}
