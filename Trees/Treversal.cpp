517d5317e3bf71f3e5500046cc957673e80b2410
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2024-11-10 15:57:35 +0530

    Update Treversal.cpp

---
+++ Trees/Treversal.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+#include "treeNode.h"
+#include <queue>
+
+void preorder(TreeNode<int> *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    cout << root->data << " ";
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        preorder(root->children[i]);
+    }
+}
+
+TreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "enter the root data :" << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int> *> pendingNodesQueue;
+    pendingNodesQueue.push(root);
+
+    while (pendingNodesQueue.size() != 0)
+    {
+        TreeNode<int> *front = pendingNodesQueue.front();
+        pendingNodesQueue.pop();
+
+        cout << "enter number of children of" << front->data << endl;
+        int numchild;
+        cin >> numchild;
+
+        for (int i = 0; i < numchild; i++)
+        {
+            cout << "enter the " << i << "th child of " << front->data << endl;
+            int childData;
+            cin >> childData;
+
+            TreeNode<int> *Child = new TreeNode<int>(childData);
+            front->children.push_back(Child);
+            pendingNodesQueue.push(Child);
+        }
+    }
+    return root;
+}
+
+int main()
+{
+    TreeNode<int> *root = takeInputLevelWise();
+}
