d82a3342f61b2f783543f3aaddfdeb9ce12f925b
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2024-11-10 13:38:34 +0530

    Update CountNodes.cpp

--- Trees/CountNodes.cpp
+++ Trees/CountNodes.cpp
@@ -15,6 +15,25 @@ int NumNodes(TreeNode<int> *root)
 }
 
 // HEIGHT OF THE TREE
+int getHeight(TreeNode<int> *root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+
+    int height = 0;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        int childHeight = getHeight(root->children[i]);
+        if (childHeight > height)
+        {
+            height = childHeight;
+        }
+    }
+    return height + 1;
+}
 
 void printLevelWiseTree(TreeNode<int> *root)
 {
