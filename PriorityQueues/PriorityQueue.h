e6ca7eb51321b7116c3adb63eae65dce7ce50323
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2025-01-04 12:30:42 +0530

    update

--- PriorityQueues/PriorityQueue.h
+++ PriorityQueues/PriorityQueue.h
@@ -10,8 +10,8 @@ public:
     // constructore
     PriorityQueue()
     {
-        int size = 0;
-        int nextIndex = 0;
+        // int size = 0;
+        // int nextIndex = 0;
     }
 
     bool isEmpty()
@@ -59,5 +59,48 @@ public:
 
     int removeMin()
     {
+        if (isEmpty())
+        {
+            return 0;
+        }
+
+        int ans = pq[0];
+        pq[0] = pq[pq.size() - 1];
+        // last element of our priority queue is deleted from the queue
+        pq.pop_back();
+
+        // down heapify
+        int parentIndex = 0;
+        int leftChildIndex = 2 * parentIndex + 1;
+        int rightChildIndex = 2 * parentIndex + 2;
+
+        while (leftChildIndex < pq.size())
+        {
+            int minIndex = parentIndex;
+
+            if (pq[minIndex] > pq[leftChildIndex])
+            {
+                minIndex = leftChildIndex;
+            }
+            if (rightChildIndex < pq.size() && pq[rightChildIndex] < pq[minIndex])
+            {
+                minIndex = rightChildIndex;
+            }
+            if (minIndex == parentIndex)
+            {
+                break;
+            }
+
+            // SWAP
+            int temp = pq[minIndex];
+            pq[minIndex] = pq[parentIndex];
+            pq[parentIndex] = temp;
+            // updating values
+            parentIndex = minIndex;
+            leftChildIndex = 2 * parentIndex + 1;
+            rightChildIndex = 2 * parentIndex + 2;
+        }
+
+        return ans;
     }
 };
--- PriorityQueues/removeMin.cpp
+++ PriorityQueues/removeMin.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
-    // PriorityQueue p;
+    PriorityQueue p;
+
+    p.insert(100);
+    p.insert(10);
+    p.insert(15);
+    p.insert(4);
+    p.insert(17);
+    p.insert(21);
+    p.insert(67);
+
+    cout << p.getSize() << endl;
+    cout << p.getmin() << endl;
+
+    while (!p.isEmpty())
+    {
+        cout << p.removeMin() << " ";
+    }
+    cout << endl;
 }
