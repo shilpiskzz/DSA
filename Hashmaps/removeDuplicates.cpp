c5967ba0d94f2e794fb28c3afbc1311189b9d338
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2025-01-06 12:53:22 +0530

    Update removeDuplicates.cpp

--- Hashmaps/removeDuplicates.cpp
+++ Hashmaps/removeDuplicates.cpp
@@ -10,16 +10,21 @@ vector<int> removeDuplicates(int *a, int size)
     // a vector to store the output
     vector<int> output;
 
+    // iterating throughout the array
     for (int i = 0; i < size; i++)
     {
         // the count fuction uses normal brackets
         if (seen.count(a[i]) > 0)
         {
+            // if the element is already present in map then proceed without doing anything, ie out of the loop
             continue;
         }
+        // we will mark the element true in the map
         seen[a[i]] = true;
+        // insert the element in the output array
         output.push_back(a[i]);
     }
+
     return output;
 }
 
